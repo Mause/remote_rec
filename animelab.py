54a19bdc367621f43538ff55e1968be5b35a0352
Dominic May <me@mause.me>
2019-01-26 11:15:15 +0800

    Add backends

---
+++ animelab.py
@@ -0,0 +1,120 @@
+import logging
+from pprint import pprint
+from typing import Optional, Dict
+from datetime import datetime
+from functools import lru_cache
+from urllib.parse import urlencode, parse_qsl
+
+import requests
+from pychromecast import Chromecast
+from pychromecast.controllers.media import MediaController
+from requests_toolbelt.sessions import BaseUrlSession
+
+USERNAME = os.environ['ANIMELAB_USERNAME']
+PASSWORD = os.environ['ANIMELAB_PASSWORD']
+ANIMELAB_APP_ID = "53C25447"
+
+session = BaseUrlSession("https://api.animelab.com/api/")
+
+
+@lru_cache()
+def get_controller(tv):
+    mc = AnimelabController()
+
+    tv.register_handler(mc)
+
+    return mc
+
+
+class AnimelabController(MediaController):
+    def __init__(self):
+        super().__init__()
+        self.app_id = ANIMELAB_APP_ID
+
+
+def autocomplete(text: str) -> Optional[Dict]:
+    result = session.get("shows/search", params={"searchTerms": text}).json()["list"]
+
+    return result[0] if result else None
+
+
+def play_show(tv: Chromecast, text: str):
+    show = autocomplete(text)
+    if not show:
+        logging.info(f'could not find a show that matched "{text}" for animelab')
+        return
+
+    logging.info(f"playing from {show['name']}")
+
+    video = get_video_for_show(show["id"])
+
+    mc = get_controller(tv)
+
+    mc.play_media(video, "video/mp4")
+    mc.block_until_active()
+
+    mc.play()
+
+
+def get_expiry(play_session: str) -> datetime:
+    _, data_bit = play_session.split("-", 1)
+    data = dict(parse_qsl(data_bit))
+
+    exp = datetime.fromtimestamp(int(data["pa.u.exp"]) / 1000)
+
+    logging.info(exp)
+
+    return exp
+
+
+def get_session():
+    play_session = session.cookies.get("PLAY_SESSION")
+    if not play_session or datetime.now() > get_expiry(play_session):
+        logging.info("obtaining new token")
+
+        session.post(
+            "login",
+            data={
+                "email": USERNAME,
+                "password": PASSWORD,
+                "rememberMe": "true",
+            },
+        )
+
+    return session
+
+
+def get_video_for_show(show_id: str) -> str:
+    first_unwatched = (
+        get_session().get(f"shows/firstunwatched/{show_id}").json()["videoList"]
+    )
+
+    logging.info(
+        {
+            fw["videoQuality"]["videoFormat"]["name"]
+            for video in first_unwatched
+            for fw in video["videoInstances"]
+        }
+    )
+
+    first_unwatched = list(
+        fw
+        for video in first_unwatched
+        for fw in video["videoInstances"]
+        if fw["videoQuality"]["videoFormat"]["name"] in {"MP4", "MPEG-DASH"}
+    )
+
+    first_unwatched = max(first_unwatched, key=lambda show: show["bitrate"])
+
+    return (
+        get_session()
+        .get(
+            f"videoinstances/sign/{first_unwatched['id']}/{first_unwatched['filename']}"
+        )
+        .json()["httpUrl"]
+    )
+
+
+if __name__ == "__main__":
+    pprint(get_video_for_show())
+
---
+++ plex.py
@@ -0,0 +1,126 @@
+import os
+import json
+from functools import lru_cache
+from typing import Optional, Any
+from urllib.parse import urlparse
+
+from pychromecast import Chromecast
+from pychromecast.controllers.media import MediaController
+from plexapi.server import PlexServer
+from plexapi.myplex import MyPlexAccount
+from plexapi.exceptions import NotFound
+from plexapi.video import Movie
+
+USERNAME = os.environ['PLEX_USERNAME']
+PASSWORD = os.environ['PLEX_PASSWORD']
+APP_ID = "9AC194DC"
+
+
+@lru_cache()
+def get_plex():
+    logging.info("connecting to plex")
+    plex = MyPlexAccount(USERNAME, PASSWORD).resource("Novell").connect()
+    logging.info("connected to plex")
+
+    return plex
+
+
+class PlexMediaController(MediaController):
+    def __init__(self, plex):
+        super().__init__()
+        self.plex = plex
+        self.supporting_app_id = APP_ID
+
+    def build_server(self):
+        url = urlparse(self.plex.url(""))
+
+        return {
+            "machineIdentifier": self.plex.machineIdentifier,
+            "protocol": url.scheme,
+            "address": url.hostname,
+            "port": url.port,
+            "transcoderVideo": self.plex.transcoderVideo,
+            "transcoderVideoRemuxOnly": False,
+            "transcoderAudio": self.plex.transcoderAudio,
+            "version": self.plex.version,
+            "myPlexSubscription": self.plex.myPlexSubscription,
+            "isVerifiedHostname": True,
+            "accessToken": self.plex._token,
+        }
+
+
+    def play_media(self, video: Movie) -> None:
+        self.launch(lambda: self._play_media(video))
+
+    def _play_media(self, video: Movie) -> None:
+        server = self.build_server()
+
+        play_queue = self.plex.createPlayQueue(video)
+
+        customData = {
+            "playQueueType": "video",
+            "providerIdentifier": "com.plexapp.plugins.library",
+            "containerKey": f"/playQueues/{play_queue.playQueueID}?own=1",
+            "offset": 0,
+            "directPlay": True,
+            "directStream": True,
+            "mediaIndex": None,
+            "audioBoost": 100,
+            "subtitleSize": 100,
+            "autoPlay": True,
+            "repeat": 0,
+            "audioForceMultiChannel": False,
+            "server": server,
+            "primaryServer": server,
+            "user": {"username": self.plex.account().username},
+        }
+
+        body = {
+            "contentId": video.key,
+            "streamType": "BUFFERED",
+            "metadata": None,
+            "duration": None,
+            "tracks": None,
+            "textTrackStyle": None,
+            "customData": customData,
+        }
+
+        self.send_message(
+            {"media": body, "type": "LOAD", "autoplay": True, "current_time": None},
+            inc_session_id=True,
+        )
+
+
+def autocomplete(text: str) -> Optional[Movie]:
+    results = get_plex().search(text)
+    return results[0] if results else None
+
+
+@lru_cache()
+def get_controller(tv: Chromecast) -> PlexMediaController:
+    mc = PlexMediaController(get_plex())
+
+    tv.register_handler(mc)
+
+    return mc
+
+
+def play_show(tv: Chromecast, show: str) -> None:
+    controller = get_controller(tv)
+
+    video = autocomplete(show)
+    if not video:
+        logging.info(f'could not find matching "{show}" on Plex')
+        return
+    else:
+        logging.info(f'playing {video.title}')
+
+    controller.play_media(video)
+
+
+if __name__ == "__main__":
+    from main import get_tv
+
+    get_plex()
+    play_show(get_tv(), input('query> '))
+
